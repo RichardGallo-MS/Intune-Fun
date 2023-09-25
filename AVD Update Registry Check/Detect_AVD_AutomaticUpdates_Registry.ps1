d5bcd618c348026babe1f5effe54c5bce142ccc3
RichardGallo-MS <85750944+RichardGallo-MS@users.noreply.github.com>
2023-09-25 17:12:32 -0400

    Updating files

---
+++ AVD Update Registry Check/Detect_AVD_AutomaticUpdates_Registry.ps1
@@ -0,0 +1,27 @@
+﻿
+#check if reg key exists and set variable accordingly
+$CurrentAutoUpdatesSettingsPresent = Test-Path -Path "HKLM:\Software\Microsoft\MSRDC\Policies"
+
+
+if (!$CurrentAutoUpdatesSettingsPresent){
+    
+    $CurrentAutoUpdatesSetting = $null
+
+}
+else {
+
+    $CurrentAutoUpdatesSetting = Get-ItemProperty -Path "HKLM:\Software\Microsoft\MSRDC\Policies" -Name AutomaticUpdates -ErrorAction SilentlyContinue | Select-Object AutomaticUpdates
+
+}
+
+
+#check value of the reg item and determine if remediation is needed
+if ($CurrentAutoUpdatesSetting.AutomaticUpdates -eq 0){
+    Write-Host "Registry is correct. No changes needed!"
+    exit 0
+}
+else{
+    Write-Host "Registry is NOT correct. Changes needed!"
+    exit 1
+    
+}
---
+++ AVD Update Registry Check/Remediate_AVD_AutomaticUpdates_Registry.ps1
@@ -0,0 +1,60 @@
+﻿#get current values
+$CurrentMSRDCKeyPresent = Test-Path -Path "HKLM:\Software\Microsoft\MSRDC\Policies"
+$CurrentAutoUpdatesSetting = Get-ItemProperty -Path "HKLM:\Software\Microsoft\MSRDC\Policies" -Name AutomaticUpdates -ErrorAction SilentlyContinue | Select-Object AutomaticUpdates
+
+#case 1 - No reg key present at all
+if (!$CurrentMSRDCKeyPresent){
+    try{
+        Write-Host "Creating Reg Keys and Reg Item since it wasn't initially detected"
+        New-Item -Path "HKLM:\Software\Microsoft" -Name MSRDC
+        New-Item -Path "HKLM:\Software\Microsoft\MSRDC" -Name Policies
+        New-ItemProperty -Path "HKLM:\Software\Microsoft\MSRDC\Policies" -Name AutomaticUpdates -PropertyType DWORD -Value 0
+        Exit 0
+     }
+     Catch {
+        Write-Host "Error Creating Reg Key"
+        Write-error $_
+        Exit 1
+     }
+}
+
+#case 2 - Reg key present, but no reg item present
+elseif (!$CurrentAutoUpdatesSetting){
+    try{
+        Write-Host "Creating Reg Item since it wasn't initially detected"
+        New-ItemProperty -Path "HKLM:\Software\Microsoft\MSRDC\Policies" -Name AutomaticUpdates -PropertyType DWORD -Value 0
+        Exit 0
+     }
+     Catch {
+        Write-Host "Error Creating Reg Item"
+        Write-error $_
+        Exit 1
+     }
+}
+
+#case 3 - Reg key and reg item present, but the value was wrong
+elseif ($CurrentAutoUpdatesSetting.AutomaticUpdates -ne 0){
+    try{
+        Write-host "Reg item already exists, but the value is wrong. Fixing this now."
+        Set-ItemProperty -Path "HKLM:\Software\Microsoft\MSRDC\Policies" -Name AutomaticUpdates -Value 0
+        Exit 0
+    }
+    catch{
+        Write-Host "Error Updating Reg Item"
+        Write-error $_
+        Exit 1
+    }
+}
+
+#case 4 - All is correct
+elseif ($CurrentAutoUpdatesSetting.AutomaticUpdates -eq 0){
+    try{
+        Write-host "Registry is correct - Good to go!"
+        Exit 0
+    }
+    catch{
+        Write-Host "Error Creating Reg Item"
+        Write-error $_
+        Exit 1
+    }
+}
